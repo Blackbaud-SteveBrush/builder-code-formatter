05be09111a022179ba6b803656fcb84048c740d4
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2021-04-09 17:36:00 -0400

    Updated script

--- index.js
+++ index.js
@@ -5,32 +5,68 @@ const prettier = require('prettier');
 
 module.exports = {
   runCommand: async (command) => {
-    const configFilePath = path.join(__dirname, 'prettier/.prettierrc.json');
-    const ignorePath = path.join(__dirname, 'prettier/.prettierrc.json');
-    const config = await prettier.resolveConfig(configFilePath);
-
     if (command === 'format') {
-      const files = glob.sync('*.js,*.ts,*.json', {
-        nodir: true
-      });
+      const configFilePath = path.join(__dirname, 'prettier/.prettierrc.json');
+      const ignorePath = path.join(__dirname, 'prettier/.prettierignore');
+      const config = await prettier.resolveConfig(configFilePath);
 
-      files.forEach(file => {
-        const info = prettier.getFileInfo(file, {
+      console.log('Finding all project files.');
+      const files = glob.sync(path.join(process.cwd(), '**/*+(.js|.ts|.json)'), {
+        nodir: true,
+        ignore: ['**/node_modules/**']
+      });
+      console.log(`Done. Found ${files.length} files.`);
+
+      let numIgnored = 0;
+      let numFailed = 0;
+      let numSuccess = 0;
+
+      const promises = files.map(async (file) => {
+        const info = await prettier.getFileInfo(file, {
           ignorePath
         });
 
         if (info.ignored) {
+          console.log(`Ignoring file ${file}`);
+          numIgnored++;
           return;
         }
 
-        const formatted = prettier.format(
-          fs.readFileSync(file, { encoding: 'utf-8' }),
-          config
-        );
+        config.parser = info.inferredParser;
 
-        fs.writeFileSync(file, formatted, { encoding: 'utf-8' });
+        try {
+          const contents = fs.readFileSync(file, { encoding: 'utf-8' });
+          const formatted = prettier.format(
+            contents,
+            config
+          );
+
+          if (contents !== formatted) {
+            fs.writeFileSync(file, formatted, { encoding: 'utf-8' });
+            console.log(`Successfully formatted file ${file}`);
+            numSuccess++;
+          } else {
+            numIgnored++;
+            console.log(`File already formatted ${file}`);
+          }
+        } catch (err) {
+          numFailed++;
+          console.error(`Failed to format file: ${file}`);
+          console.error(err);
+        }
       });
+
+      await Promise.all(promises);
+
+      console.log(`
+====================================
+ SKY UX Format Results:
+====================================
+ Num. failed:    ${numFailed}
+ Num. ignored:   ${numIgnored}
+ Num. succeeded: ${numSuccess}
+------------------------------------
+`);
     }
-    // "format-all": "npx prettier --write ."
   }
 };
